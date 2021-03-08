1360e48dd72e94e84f478a2cc17c1e482170358b
JAKASHA-ctrl <63239006+JAKASHA-ctrl@users.noreply.github.com>
2021-03-08 22:45:04 +0200

    23rd commit

--- sendmail.php
+++ sendmail.php
@@ -19,15 +19,15 @@ $mail->Subject = 'Лист з сайту FILTERS';
 
 $body = '<h1>Крутий лист з сайту "Filters"!</h1>';
 
-if (trim(!empty($_POST['name']))){
-  $body .= '<p><strong>Ім"я:</strong> '.$_POST['name'].' </p>';
-}
-if (trim(!empty($_POST['email']))){
-  $body .= '<p><strong>E-mail</strong> '.$_POST['email'].' </p>';
-}
-if (trim(!empty($_POST['message']))){
-  $body .= '<p><strong>Повідомлення:</strong> '.$_POST['message'].' </p>';
-}
+// if (trim(!empty($_POST['name']))){
+//   $body .= '<p><strong>Ім"я:</strong> '.$_POST['name'].' </p>';
+// }
+// if (trim(!empty($_POST['email']))){
+//   $body .= '<p><strong>E-mail</strong> '.$_POST['email'].' </p>';
+// }
+// if (trim(!empty($_POST['message']))){
+//   $body .= '<p><strong>Повідомлення:</strong> '.$_POST['message'].' </p>';
+// }
 
 $mail->Body = $body;
 
